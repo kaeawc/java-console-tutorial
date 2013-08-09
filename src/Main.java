8ee1573254e7e4cf9c69ca8b156239fdb0bc855e
Jason Pearson <jason.d.pearson@gmail.com>
2013-08-08 19:30:50 -0600

    printing out args

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello World!");
+    StringBuilder list = new StringBuilder();
+
+    for (String arg : args) list.append(arg);
+
+    System.out.printf("Here are my args: %s", list.toString());
 
   }
 }
