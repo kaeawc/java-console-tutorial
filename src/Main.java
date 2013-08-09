66c22c202f2b56d9edc62da7b7f1a57c81c517a9
Jason Pearson <jason.d.pearson@gmail.com>
2013-08-08 19:41:33 -0600

    added looping on user input

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
 
     StringBuilder list = new StringBuilder();
 
+    String prompt = "Do you want to do something? (y/n)";
+
     for (String arg : args) list.append(arg);
 
     System.out.printf("Here are my args: %s", list.toString());
+
+    System.out.println(prompt);
+
+    while(!new Scanner(System.in).nextLine().equals("n")) {
+
+      System.out.println("Doing something...");
+
+      System.out.println(prompt);
+
+    }
 
   }
 }
